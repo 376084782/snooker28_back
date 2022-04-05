5be05a2fabeea4dc41bdf114a7de00590dcfb6a8
qianliyun <376084782@qq.com>
2022-04-05 14:09:40 +0800

    no message

--- src/mongodb/data.ts
+++ src/mongodb/data.ts
@@ -66,7 +66,7 @@ async function initRoomConfig() {
 
 
 async function initUser() {
-  const listUser = [
+  let listUser = [
     { tagCheat: true, coin: 200000, uid: '111', nickname: '钱最多', avatar: 'https://img0.baidu.com/it/u=16966295,3736937037&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500' },
     { tagCheat: false, coin: 300000, uid: '112', nickname: '钱很少', avatar: 'https://img2.baidu.com/it/u=2078308964,2142755897&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400' },
     { tagCheat: false, coin: 10000000, uid: '113', nickname: '钱很多', avatar: 'https://img2.baidu.com/it/u=2391726625,2951775714&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500' },
@@ -74,6 +74,11 @@ async function initUser() {
     { tagCheat: false, coin: 10000000, uid: '115', nickname: '输很多', avatar: 'https://img2.baidu.com/it/u=2464854331,2113352486&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400' },
     { tagCheat: false, coin: 10000000, uid: '116', nickname: '输狂多', avatar: 'https://img2.baidu.com/it/u=80344671,2129607677&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400' },
   ];
+  for (let i = 117; i < 200; i++) {
+    listUser.push({
+      tagCheat: false, coin: 10000000, uid: '' + i, nickname: '测试玩家' + i, avatar: 'https://img2.baidu.com/it/u=80344671,2129607677&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400'
+    })
+  }
   await ModelUser.deleteMany();
   ModelUser.create(listUser)
 }
